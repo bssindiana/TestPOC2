89cfd55c41309c37517bd51facc228473dc08471
CORP\bsenshar <bishwajeet-sen-sharma@capgemini.com>
2019-01-18 19:03:55 +0530

    Commiting for second time

--- proj1/anotherClass.java
+++ proj1/anotherClass.java
@@ -10,4 +10,4 @@ public static void main( String args[])
 System.out.println("Hi World");
 }// End of method
 }//End of class
-
+//New Comment
--- proj1/test1.java
+++ proj1/test1.java
@@ -1,13 +1,1 @@
-package pak1
-
-import java.lang.*;
-
-class main
-{
-public static void main( String args[])
-{
-System.out.println("Hi World");
-}// End of method
-}//End of class
-
-
+//adding additional comments
